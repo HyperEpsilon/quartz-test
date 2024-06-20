b2b36791916b59ae86ef2acf69a1764dccc05195
HyperEpsilon <6267244+HyperEpsilon@users.noreply.github.com>
2024-06-20 16:40:44 -0600

    add explicit publish flag plugin

--- quartz.config.ts
+++ quartz.config.ts
@@ -70,7 +70,7 @@ const config: QuartzConfig = {
       Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
       Plugin.Description(),
     ],
-    filters: [Plugin.RemoveDrafts()],
+    filters: [Plugin.RemoveDrafts(), Plugin.ExplicitPublish()],
     emitters: [
       Plugin.AliasRedirects(),
       Plugin.ComponentResources(),
